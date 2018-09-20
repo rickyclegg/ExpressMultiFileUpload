465cab6514cd5b5a7c092ae9ed93ea0a06c0c9fa
Ricky Clegg <me@rickyclegg.com>
2018-09-20 13:05:41 +0100

    Fix npm scripts

--- karma.conf.js
+++ karma.conf.js
@@ -1,68 +1,84 @@
 /* eslint-disable sort-keys,array-bracket-newline */
-// Karma configuration
-// Generated on Wed Sep 19 2018 21:36:49 GMT+0100 (British Summer Time)
+/*
+ * Karma configuration
+ * Generated on Wed Sep 19 2018 21:36:49 GMT+0100 (British Summer Time)
+ */
 
 module.exports = config => {
   config.set({
 
-    // base path that will be used to resolve all patterns (eg. files, exclude)
+    // Base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
 
 
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    /*
+     * Frameworks to use
+     * available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+     */
     frameworks: ['jasmine-jquery', 'jasmine'],
 
 
-    // list of files / patterns to load in the browser
+    // List of files / patterns to load in the browser
     files: [
       'public/src/**/*.js'
     ],
 
 
-    // list of files / patterns to exclude
+    // List of files / patterns to exclude
     exclude: [],
 
 
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    /*
+     * Preprocess matching files before serving them to the browser
+     * available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+     */
     preprocessors: {},
 
 
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    /*
+     * Test results reporter to use
+     * possible values: 'dots', 'progress'
+     * available reporters: https://npmjs.org/browse/keyword/karma-reporter
+     */
     reporters: ['progress'],
 
 
-    // web server port
+    // Web server port
     port: 9876,
 
 
-    // enable / disable colors in the output (reporters and logs)
+    // Enable / disable colors in the output (reporters and logs)
     colors: true,
 
 
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    /*
+     * Level of logging
+     * possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+     */
     logLevel: config.LOG_INFO,
 
 
-    // enable / disable watching file and executing tests whenever any file changes
+    // Enable / disable watching file and executing tests whenever any file changes
     autoWatch: false,
 
 
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    /*
+     * Start these browsers
+     * available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+     */
     browsers: ['Chrome'],
 
 
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
+    /*
+     * Continuous Integration mode
+     * if true, Karma captures browsers, runs the tests and exits
+     */
     singleRun: true,
 
-    // Concurrency level
-    // how many browser should be started simultaneous
+    /*
+     * Concurrency level
+     * how many browser should be started simultaneous
+     */
     concurrency: Infinity
   });
 };
