c34c522d914b6a62b5a4b555ac69c935c67560d7
Ricky Clegg <me@rickyclegg.com>
2018-09-21 14:54:31 +0100

    Add missing test

--- e2e/api/upload/post.spec.js
+++ e2e/api/upload/post.spec.js
@@ -14,7 +14,25 @@ describe('Route: upload', () => {
       server.listening.then(done);
     });
 
-    it('should upload files and place on server', (done) => {
+    it('should return successful 201 with uploaded filenames', (done) => {
+      request(server)
+        .post(endpoint)
+        .attach('uploads[]', './e2e/fixtures/file1.pdf')
+        .attach('uploads[]', './e2e/fixtures/file2.pdf')
+        .expect(constants.server.responseCode.CREATED)
+        .then(res => {
+          const filenames = JSON.parse(res.text);
+
+          expect(filenames[0]).toEqual('file1.pdf');
+          expect(filenames[1]).toEqual('file2.pdf');
+          expect(filenames.length).toEqual(2);
+
+          done();
+        })
+        .catch(done.fail);
+    });
+
+    it('should upload files and place insert in db', (done) => {
       request(server)
         .post(endpoint)
         .attach('uploads[]', './e2e/fixtures/file1.pdf')
@@ -26,7 +44,8 @@ describe('Route: upload', () => {
           expect(results.rows[0].filename).toEqual('file1.pdf');
 
           done();
-        });
+        })
+        .catch(done.fail);
     });
   });
 });
